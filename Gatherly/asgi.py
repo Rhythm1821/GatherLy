ec4c345ab3b2a7d837f0b3c460e2a81c1f4e0466
Rhythm1821 <alamrawat1821@gmail.com>
2024-08-18 19:38:02 +0530

    Updates for deployment

--- Gatherly/asgi.py
+++ Gatherly/asgi.py
@@ -1,18 +1,19 @@
 import os
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Gatherly.settings')
+import django
+django.setup()
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-from django.core.asgi import get_asgi_application
-
 from room.routing import websocket_urlpatterns
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Gatherly.settings')
 
 application = ProtocolTypeRouter(
     {
-        "http":get_asgi_application(),
-        "websocket":AllowedHostsOriginValidator(
+        "http": get_asgi_application(),
+        "websocket": AllowedHostsOriginValidator(
             AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
         ),
     }
